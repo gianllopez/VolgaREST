d4b971899ec5ddcfe6620b8773185a54f4aeb9cc
Gian Lopez <lopezarizagianlucas@gmail.com>
2021-01-10 23:29:02 -0500

    Contact: agregados los utils y creo que funcionando, hay que agregar las variables de entorno cuando haga el deploy a Heroku.

---
+++ VolgaREST/endpoints/utils/constants.py
@@ -0,0 +1,71 @@
+ARGUMENTS = [
+   'headless',
+   'disable-dev-shm-usage',
+   'disable-notifications',
+   'no-sandbox',
+   'user-agent=Chrome/87.0.4280.141',
+   'log-level=3'
+]
+
+INSTAGRAM = {
+   'root-url': 'https://www.instagram.com',
+   'login': {
+      'path': '/accounts/login',
+      'form': {
+         'input[name="username"]': 'gianlop3z',
+         'input[name="password"]': 'al capone'
+      },
+      'submit-btn': '#loginForm button.sqdOP.L3NKy.y3zKF',
+      'session-cookie': 'sessionid'
+   },
+   'message': {
+      # 'to': 'gianlop3z', / This must be added in contact function.
+      'DOM-to-use': {
+         'follow-btn': 'div.Igw0E.IwRSH.eGOV_.ybXk5._4EzTm button',
+         'redirect-btn': 'div._862NM button',
+         'message-input': 'div.Igw0E.IwRSH.eGOV_.vwCYk.ItkAi > textarea'
+      }
+   }
+}
+
+FACEBOOK = {
+   'root-url': 'https://www.facebook.com',
+   'login': {
+      'form': {
+         'input[name="email"]': 'lopezarizagianlucas@gmail.com',
+         'input[name="pass"]': 'megazepol04'
+      },
+      'submit-btn': '#loginbutton',
+      'session-cookie': 'c_user'
+   },
+   'message': {
+      # 'to': 'therealgoat01', / This must be added in contact function.
+      'DOM-to-use': {
+         'add-btn': '.bp9cbjyn.j83agx80.fop5sh7t div[aria-label="Add Friend"]',
+         'message-input': 'div[data-pagelet="ChatTab"] div._1mf._1mj'
+      }
+   }
+}
+
+TWITTER = {
+   'root-url': 'https://twitter.com',
+   'login': {
+      'form': {
+         'input[name="session[username_or_email]"].r-30o5oe': 'gianlop3z',
+         'input[name="session[password]"].r-30o5oe': 'megazepol'
+      },
+      'submit-btn': 'div[data-testid="LoginForm_Login_Button"]',
+      'session-cookie': 'auth_token'
+   },
+   'message': {
+      # 'to': 'gianlop3z', / This must be added in contact function.
+      'DOM-to-use': {
+         'follow-btn': 'div[data-testid="323485251-follow"]',
+         'message-input': '.public-DraftStyleDefault-block'
+      }
+   }
+}
+
+def CODE_MESSAGE(code):
+   return 'Gracias por registrate con Volga, este es tu código de autenticación para esta red de contacto: {code}'
+
---
+++ VolgaREST/endpoints/utils/contact_auth.py
@@ -0,0 +1,65 @@
+from selenium.webdriver.support.expected_conditions import presence_of_element_located as appears
+from selenium.common.exceptions import TimeoutException, ElementClickInterceptedException
+from selenium.webdriver.support.wait import WebDriverWait as WaitFor
+from selenium.webdriver import Chrome, ChromeOptions
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.common.by import By
+from time import sleep as waitDOM
+from constants import *
+
+class ContactAuthentication:
+   
+   def __init__(self):
+      options = ChromeOptions()
+      for x in ARGUMENTS:
+         arg = f'--{x}' if ARGUMENTS.index(x) <= 3 else x
+         options.add_argument(arg)
+      self.driver = Chrome(executable_path=executable, options=options)
+   def destructure(self, data):
+      return [x[1] for x in sorted(data.items())]
+   
+   def querySelector(self, selector, timeout=15):
+      return WaitFor(driver=self.driver, timeout=timeout).until(appears((By.CSS_SELECTOR, selector)))
+
+   def login(self, root, data):
+      data['path'] = data.get('path', '/login')
+      global session_cookie
+      credentials, path, session_cookie, submit_btn = self.destructure(data)
+      self.driver.get(root + path)
+      for x in credentials:
+         self.querySelector(x).send_keys(credentials[x])
+      self.querySelector(submit_btn).click()
+   
+   def send_code(self, contact_data, user, authcode):
+      login_data, message_data, root_url = self.destructure(contact_data)
+      message_data['to'] = user
+      self.login(root=root_url, data=login_data)
+      while True:
+         if self.driver.get_cookie(session_cookie):
+            btns, to = self.destructure(message_data)
+            waitDOM(0.5)
+            self.driver.get(f'{root_url}/{to}')
+            break
+      btns['redirect-btn'] = btns.get('redirect-btn', 'div[aria-label="Mensaje"], div[aria-label="Message"]')
+      follow_or_add, msg_entry, msg_redirect = self.destructure(btns)
+      try:
+         self.querySelector(follow_or_add, timeout=3).click()
+         self.querySelector(msg_redirect).click()
+      except (TimeoutException, ElementClickInterceptedException):
+         pass
+      self.querySelector(msg_entry).send_keys(CODE_MESSAGE(authcode), Keys.ENTER)
+
+   def instagram(self, to, code):
+      self.send_code(contact_data=INSTAGRAM, user=to, authcode=code)
+   
+   def facebook(self, to, code):
+      self.send_code(contact_data=FACEBOOK, user=to, authcode=code)
+   
+   def whatsapp(self):
+      pass
+
+   def twitter(self, to, code):
+      self.send_code(contact_data=TWITTER, user=to, authcode=code)
+      
+   def email(self):
+      pass
