4fd9ab640860759ea01fe2c158ae03cc0ad681ff
Gian Lopez <lopezarizagianlucas@gmail.com>
2021-01-15 11:58:38 -0500

    ContactNetworks: eliminado Selenoum y sus dependencias.

--- VolgaREST/endpoints/serializers/contact.py
+++ VolgaREST/endpoints/serializers/contact.py
@@ -1,19 +1,7 @@
 from rest_framework.serializers import ModelSerializer
 from VolgaREST.root.models import ContactModel, UserModel
-from ..utils.contact_auth import ContactAuthentication
 
 class ContactSerializer(ModelSerializer):
-   
    class Meta:
       model = ContactModel
       fields = '__all__'
-   
-   def validate(self, data):
-      # import pdb; pdb.set_trace()
-      contacts = {}
-      for x in data:
-         if x not in ['token', 'whatsapp']:
-            contacts[x] = data[x]
-      auth = ContactAuthentication(credentials=contacts)
-      auth.execute()
-      return data
