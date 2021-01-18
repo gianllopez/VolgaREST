1b6c96538220c19266b9e7760cf5d0f822d87faa
Gian Lopez <lopezarizagianlucas@gmail.com>
2021-01-17 20:17:47 -0500

    Logup: agregado el solo POST request al viewset y al diablo con el superuser de django.

--- VolgaREST/endpoints/viewsets/logup.py
+++ VolgaREST/endpoints/viewsets/logup.py
@@ -1,14 +1,16 @@
-from rest_framework.viewsets import ModelViewSet
+from rest_framework.viewsets import GenericViewSet
 from rest_framework.response import Response
 from rest_framework.status import HTTP_201_CREATED
 from rest_framework.authtoken.models import Token
+from rest_framework.mixins import CreateModelMixin
 from ..serializers import LogupSerializer
 from VolgaREST.root.models import UserModel
 
-class LogupViewSet(ModelViewSet):
+class LogupViewSet(GenericViewSet, CreateModelMixin):
    
    serializer_class = LogupSerializer
    queryset = UserModel.objects.all()
+   authentication_classes = permission_classes = []
 
    def create(self, request):
       serializer = self.serializer_class(data=request.data)
