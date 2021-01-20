04c865ae3bc05ce2996d2583c3d438008b0c3668
Gian Lopez <lopezarizagianlucas@gmail.com>
2021-01-19 19:17:32 -0500

    All: endpoints listos hasta el momento...

--- VolgaREST/endpoints/serializers/user_profile_picture.py
+++ VolgaREST/endpoints/serializers/user_profile_picture.py
@@ -1,22 +1,12 @@
-from rest_framework.serializers import Serializer, CharField, ImageField
+from rest_framework.serializers import ModelSerializer
 from VolgaREST.root.models import UserModel
-from cloudinary.uploader import upload
 
-class UserProfilePictureSerializer(Serializer):
-   
-   username = CharField(max_length=25)
-   picture = ImageField(allow_empty_file=True)
+class UserProfilePictureSerializer(ModelSerializer):
+   class Meta:
+      model = UserModel
+      fields = [
+         'username',
+         'picture'
+      ]
 
-   def create(self, validated_data):
-      username, loadedpic = validated_data.values()
-      user = UserModel.objects.get(username=username)
-      if loadedpic:
-         picture = upload(
-            file=loadedpic.file, folder='profile-pictures/',
-            public_id=username, overwrite=True)
-         user.picture = picture['secure_url']
-         user.save()
-      else:
-         baseurl = 'https://res.cloudinary.com/volga/image/upload/v1611089503/blankpp-'
-         loadedpic = baseurl + 
-      return validated_data
+
--- VolgaREST/endpoints/viewsets/user_profile_picture.py
+++ VolgaREST/endpoints/viewsets/user_profile_picture.py
@@ -1,8 +1,34 @@
 from rest_framework.viewsets import GenericViewSet
 from rest_framework.mixins import CreateModelMixin
+from rest_framework.status import HTTP_201_CREATED
+from rest_framework.response import Response
 from ..serializers import UserProfilePictureSerializer
 from VolgaREST.root.models import UserModel
+from cloudinary.uploader import upload
 
 class UserProfilePictureViewSet(GenericViewSet, CreateModelMixin):
    serializer_class = UserProfilePictureSerializer
    queryset = UserModel.objects.all()
+
+   def get_blankpp(self, gender):
+         genderspp = {
+            'Masculino': 'blankpp-men.png',
+            'Masculino': 'blankpp-women.png',
+            'No definido': 'blankpp-undefined.png',
+            'Prefiero no especificarlo': 'blankpp-undefined.png'
+         }
+         baseurl = 'https://res.cloudinary.com/volga/image/upload/v1611089503/'
+         return baseurl + genderspp[gender]
+         
+   def create(self, request):
+      username, loadedpic = request.data.values()
+      user = UserModel.objects.get(username=username)
+      if loadedpic:
+         picture = upload (
+            file=loadedpic.file, folder='profile-pictures/',
+            public_id=username, overwrite=True)
+         user.picture = picture['secure_url']
+      else:
+         user.picture = self.get_blankpp(gender=user.gender)
+      user.save()
+      return Response(data={'profile-picture': user.picture}, status=HTTP_201_CREATED)
