7fd02d3917eebce24d917dd4ece1d10b3bf980ed
Gian Lopez <lopezarizagianlucas@gmail.com>
2021-01-22 22:40:06 -0500

    ClientOpinions: endpoint casi ready, luego sigo...

--- VolgaREST/root/models/clients_opinions.py
+++ VolgaREST/root/models/clients_opinions.py
@@ -3,10 +3,13 @@ from .user import UserModel
 
 class ClientsOpinionsModel(models.Model):
 
-   user = models.ForeignKey(to=UserModel, on_delete=models.CASCADE)
+   user = models.ForeignKey (
+      to=UserModel,
+      on_delete=models.CASCADE)
 
    rating = models.IntegerField(default=10)
    client = models.CharField(max_length=75)
+   email = models.EmailField(max_length=100)
    comment = models.TextField(max_length=125)
 
    def __str__(self):
