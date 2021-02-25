194719aefb006c28e9a6b1e016a9614de59b4969
Gian Lopez <lopezarizagianlucas@gmail.com>
2021-02-24 22:52:14 -0500

    Resolviendo el error de contact networks, resumiendo.

--- VolgaREST/endpoints/viewsets/contact_networks.py
+++ VolgaREST/endpoints/viewsets/contact_networks.py
@@ -9,9 +9,35 @@ class ContactNetworksViewSet(ModelViewSet):
    serializer_class = ContactNetworksSerializer
    queryset = ContactNetworksModel.objects.all()
 
+   def get_complete_url(self, network, contact):
+      if contact:
+         urls = {
+            'instagram': 'https://www.instagram.com/{}',
+            'facebook': 'https://www.facebook.com/{}',
+            'whatsapp': 'https://wa.me/{}',
+            'twitter': 'https://www.twitter.com/{}',
+            'email': 'mailto:{}',
+            'linkedin': 'https://www.linkedin.com/in/{}'}
+         return urls[network].format(contact)
+
+
+
+         
+
+         
+
+         url = f'https://wa.me/{contact}' if network == 'whatsapp' else 'mailto:' + contact
+         if network in ['facebook', 'instagram', 'twitter', 'linkedin']:
+            cn = 'in/' + contact if network == 'linkedin' else contact
+            url = f'https://www.{network}.com/{cn}'
+         return url
+
    def create(self, request):
-      request.data['user'] = request.__dict__['_user']
-      serializer = self.serializer_class(data=request.data)
+      data = request.data
+      for field in data:
+         data[field] = self.get_complete_url(field, data[field])
+      data['user'] = request.__dict__['_user']
+      serializer = self.serializer_class(data=data)
       serializer.is_valid(raise_exception=True)
       serializer.save()
       return Response(status=HTTP_201_CREATED)
