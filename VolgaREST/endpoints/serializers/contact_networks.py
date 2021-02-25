8566e34ae85313b08ac96831033fc90b4f88f8b4
Gian Lopez <lopezarizagianlucas@gmail.com>
2021-02-24 22:52:14 -0500

    Resolviendo el error de contact networks, resumiendo.

--- VolgaREST/endpoints/serializers/contact_networks.py
+++ VolgaREST/endpoints/serializers/contact_networks.py
@@ -5,18 +5,3 @@ class ContactNetworksSerializer(ModelSerializer):
    class Meta:
       model = ContactNetworksModel
       fields = '__all__'
-
-   def get_complete_url(self, network, contact):
-      if contact:
-         if network in ['facebook', 'instagram', 'twitter', 'linkedin']:
-            cn = 'in/' + contact if network == 'linkedin' else contact
-            return f'https://www.{network}.com/{cn}'
-         else:
-            if network != 'user':
-               return f'https://wa.me/{contact}' if network == 'whatsapp' else 'mailto:' + contact
-
-   def create(self, validated_data):
-      for data in validated_data:
-         if data != 'user':
-            validated_data[data] = self.get_complete_url(data, validated_data[data])
-      return ContactNetworksModel.objects.create(**validated_data)
--- VolgaREST/endpoints/viewsets/contact_networks.py
+++ VolgaREST/endpoints/viewsets/contact_networks.py
@@ -9,9 +9,21 @@ class ContactNetworksViewSet(ModelViewSet):
    serializer_class = ContactNetworksSerializer
    queryset = ContactNetworksModel.objects.all()
 
+   def get_complete_url(self, network, contact):
+      if contact:
+         c_urls = ['instagram', 'facebook', 'twitter']
+         urls = {a: 'https://www.{}.com/'.format(b) + '{}' for (a, b) in zip(c_urls, c_urls)}
+         urls['whatsapp'] = 'https://wa.me/{}'
+         urls['linkedin'] = 'https://www.linkedin.com/in/{}' 
+         return urls[network].format(contact)
+
    def create(self, request):
-      request.data['user'] = request.__dict__['_user']
-      serializer = self.serializer_class(data=request.data)
+      data = request.data
+      for field in data:
+         if field != 'email':
+            data[field] = self.get_complete_url(field, data[field])
+      data['user'] = request.__dict__['_user']
+      serializer = self.serializer_class(data=data)
       serializer.is_valid(raise_exception=True)
       serializer.save()
       return Response(status=HTTP_201_CREATED)
