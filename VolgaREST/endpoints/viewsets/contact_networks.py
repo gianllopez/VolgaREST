048c17aa36922ab752a2aed9bb86b2970e5378c2
Gian Lopez <lopezarizagianlucas@gmail.com>
2021-02-27 18:32:48 -0500

    Medio cambiado el viewset del contact networks.

--- VolgaREST/endpoints/viewsets/contact_networks.py
+++ VolgaREST/endpoints/viewsets/contact_networks.py
@@ -1,5 +1,6 @@
 from rest_framework.response import Response
-from rest_framework.status import HTTP_201_CREATED
+from rest_framework.status import (
+   HTTP_201_CREATED, HTTP_400_BAD_REQUEST, HTTP_409_CONFLICT)
 from ..serializers import ContactNetworksSerializer
 from VolgaREST.root.models import ContactNetworksModel
 from ..custom import CreateViewSet
@@ -14,16 +15,22 @@ class ContactNetworksViewSet(CreateViewSet):
          c_urls = ['instagram', 'facebook', 'twitter']
          urls = {a: 'https://www.{}.com/'.format(b) + '{}' for (a, b) in zip(c_urls, c_urls)}
          urls['whatsapp'] = 'https://wa.me/{}'
-         urls['linkedin'] = 'https://www.linkedin.com/in/{}' 
-         return urls[network].format(contact)
+         urls['linkedin'] = 'https://www.linkedin.com/in/{}'
+         return urls[network].format(contact) if network != 'email' else contact
 
    def create(self, request):
       data = request.data
       for field in data:
          data[field] = self.get_complete_url(field, data[field])
-      data['user'] = request.__dict__['_user']
+      data['user'] = request.user
       serializer = self.serializer_class(data=data)
-      serializer.is_valid(raise_exception=True)
-      serializer.save()
-      return Response(status=HTTP_201_CREATED)
+      if serializer.is_valid():
+         serializer.save()
+         return Response(status=HTTP_201_CREATED)
+      else:
+         errors = serializer.errors
+         response = {'data': serializer.errors}
+         user = errors.get('user', None)
+         response['status'] = HTTP_409_CONFLICT if user else HTTP_400_BAD_REQUEST
+         return Response(**response)
 
