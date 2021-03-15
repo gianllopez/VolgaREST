7a4e1090c537e089e3f8b57860495dbb6dddc6fb
Gian Lopez <lopezarizagianlucas@gmail.com>
2021-03-15 13:28:13 -0500

    Hecha segunda migration desde que i started with this, sin super usuarios.

--- VolgaREST/root/models/user.py
+++ VolgaREST/root/models/user.py
@@ -26,8 +26,6 @@ class UserModel(AbstractBaseUser):
 
    verified_email = models.BooleanField(default=False)
 
-   is_superuser = models.BooleanField(default=False)
-
    USERNAME_FIELD = 'username'
    objects = BaseUserManager()
 
